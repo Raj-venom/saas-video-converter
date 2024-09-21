dfae2e9b383a9033d5b6ab8f7bb0f23b42086633
Raj-venom <rajsingh.rsr9824@gmail.com>
2024-09-21 23:40:24 +0545

    Add clerk auth and database config

---
+++ src/app/api/videos/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+
+export async function GET(req: NextRequest) {
+    try {
+        const videos = await prisma.video.findMany({
+            orderBy: { createdAt: 'desc' }
+        })
+        return NextResponse.json({ videos }, { status: 200 })
+
+    } catch (error) {
+        return NextResponse.json({ error: error }, { status: 500 })
+    } finally {
+        await prisma.$disconnect()
+    }
+}
